1948ae33014d820d616b84d13e16f746e7d99e89
JonasS <jonass@dev.jsje.de>
2023-07-05 22:54:24 +0200

    rename CLSID/IID constants to prevent clashes with predefined versions in later MinGW versions

--- src/wasapi.c
+++ src/wasapi.c
@@ -31,46 +31,46 @@
 #endif //E_NOTFOUND
 
 // And some GUID are never implemented (Ignoring the INITGUID define)
-static const CLSID CLSID_MMDeviceEnumerator = {
+static const CLSID soundio_CLSID_MMDeviceEnumerator = {
     0xbcde0395, 0xe52f, 0x467c, {0x8e, 0x3d, 0xc4, 0x57, 0x92, 0x91, 0x69, 0x2e}
 };
-static const IID   IID_IMMDeviceEnumerator   = {
+static const IID   soundio_IID_IMMDeviceEnumerator   = {
     //MIDL_INTERFACE("A95664D2-9614-4F35-A746-DE8DB63617E6")
     0xa95664d2, 0x9614, 0x4f35, {0xa7, 0x46, 0xde, 0x8d, 0xb6, 0x36, 0x17, 0xe6}
 };
-static const IID   IID_IMMNotificationClient = {
+static const IID   soundio_IID_IMMNotificationClient = {
     //MIDL_INTERFACE("7991EEC9-7E89-4D85-8390-6C703CEC60C0")
     0x7991eec9, 0x7e89, 0x4d85, {0x83, 0x90, 0x6c, 0x70, 0x3c, 0xec, 0x60, 0xc0}
 };
-static const IID   IID_IAudioClient = {
+static const IID   soundio_IID_IAudioClient = {
     //MIDL_INTERFACE("1CB9AD4C-DBFA-4c32-B178-C2F568A703B2")
     0x1cb9ad4c, 0xdbfa, 0x4c32, {0xb1, 0x78, 0xc2, 0xf5, 0x68, 0xa7, 0x03, 0xb2}
 };
-static const IID   IID_IAudioRenderClient    = {
+static const IID   soundio_IID_IAudioRenderClient    = {
     //MIDL_INTERFACE("F294ACFC-3146-4483-A7BF-ADDCA7C260E2")
     0xf294acfc, 0x3146, 0x4483, {0xa7, 0xbf, 0xad, 0xdc, 0xa7, 0xc2, 0x60, 0xe2}
 };
-static const IID   IID_IAudioSessionControl  = {
+static const IID   soundio_IID_IAudioSessionControl  = {
     //MIDL_INTERFACE("F4B1A599-7266-4319-A8CA-E70ACB11E8CD")
     0xf4b1a599, 0x7266, 0x4319, {0xa8, 0xca, 0xe7, 0x0a, 0xcb, 0x11, 0xe8, 0xcd}
 };
-static const IID   IID_IAudioSessionEvents   = {
+static const IID   soundio_IID_IAudioSessionEvents   = {
     //MIDL_INTERFACE("24918ACC-64B3-37C1-8CA9-74A66E9957A8")
     0x24918acc, 0x64b3, 0x37c1, {0x8c, 0xa9, 0x74, 0xa6, 0x6e, 0x99, 0x57, 0xa8}
 };
-static const IID IID_IMMEndpoint = {
+static const IID soundio_IID_IMMEndpoint = {
     //MIDL_INTERFACE("1BE09788-6894-4089-8586-9A2A6C265AC5")
     0x1be09788, 0x6894, 0x4089, {0x85, 0x86, 0x9a, 0x2a, 0x6c, 0x26, 0x5a, 0xc5}
 };
-static const IID IID_IAudioClockAdjustment = {
+static const IID soundio_IID_IAudioClockAdjustment = {
     //MIDL_INTERFACE("f6e4c0a0-46d9-4fb8-be21-57a3ef2b626c")
     0xf6e4c0a0, 0x46d9, 0x4fb8, {0xbe, 0x21, 0x57, 0xa3, 0xef, 0x2b, 0x62, 0x6c}
 };
-static const IID IID_IAudioCaptureClient = {
+static const IID soundio_IID_IAudioCaptureClient = {
     //MIDL_INTERFACE("C8ADBD64-E71E-48a0-A4DE-185C395CD317")
     0xc8adbd64, 0xe71e, 0x48a0, {0xa4, 0xde, 0x18, 0x5c, 0x39, 0x5c, 0xd3, 0x17}
 };
-static const IID IID_ISimpleAudioVolume = {
+static const IID soundio_IID_ISimpleAudioVolume = {
     //MIDL_INTERFACE("87ce5498-68d6-44e5-9215-6da47ef883d8")
     0x87ce5498, 0x68d6, 0x44e5,{ 0x92, 0x15, 0x6d, 0xa4, 0x7e, 0xf8, 0x83, 0xd8 }
 };
@@ -81,15 +81,15 @@ static const IID IID_ISimpleAudioVolume = {
 #define IS_EQUAL_IID(a, b) IsEqualIID((a), *(b))
 
 // And some constants are passed by reference
-#define IID_IAUDIOCLIENT                      (IID_IAudioClient)
-#define IID_IMMENDPOINT                       (IID_IMMEndpoint)
-#define IID_IAUDIOCLOCKADJUSTMENT             (IID_IAudioClockAdjustment)
-#define IID_IAUDIOSESSIONCONTROL              (IID_IAudioSessionControl)
-#define IID_IAUDIORENDERCLIENT                (IID_IAudioRenderClient)
-#define IID_IMMDEVICEENUMERATOR               (IID_IMMDeviceEnumerator)
-#define IID_IAUDIOCAPTURECLIENT               (IID_IAudioCaptureClient)
-#define IID_ISIMPLEAUDIOVOLUME                (IID_ISimpleAudioVolume)
-#define CLSID_MMDEVICEENUMERATOR              (CLSID_MMDeviceEnumerator)
+#define soundio_IID_IAUDIOCLIENT                      (soundio_IID_IAudioClient)
+#define soundio_IID_IMMENDPOINT                       (soundio_IID_IMMEndpoint)
+#define soundio_IID_IAUDIOCLOCKADJUSTMENT             (soundio_IID_IAudioClockAdjustment)
+#define soundio_IID_IAUDIOSESSIONCONTROL              (soundio_IID_IAudioSessionControl)
+#define soundio_IID_IAUDIORENDERCLIENT                (soundio_IID_IAudioRenderClient)
+#define soundio_IID_IMMDEVICEENUMERATOR               (soundio_IID_IMMDeviceEnumerator)
+#define soundio_IID_IAUDIOCAPTURECLIENT               (soundio_IID_IAudioCaptureClient)
+#define soundio_IID_ISIMPLEAUDIOVOLUME                (soundio_IID_ISimpleAudioVolume)
+#define soundio_CLSID_MMDEVICEENUMERATOR              (soundio_CLSID_MMDeviceEnumerator)
 #define PKEY_DEVICE_FRIENDLYNAME              (PKEY_Device_FriendlyName)
 #define PKEY_AUDIOENGINE_DEVICEFORMAT         (PKEY_AudioEngine_DeviceFormat)
 
@@ -97,17 +97,17 @@ static const IID IID_ISimpleAudioVolume = {
 #define IS_EQUAL_GUID(a, b) IsEqualGUID((a), (b))
 #define IS_EQUAL_IID(a, b) IsEqualIID((a), (b))
 
-#define IID_IAUDIOCLIENT (&IID_IAudioClient)
-#define IID_IMMENDPOINT (&IID_IMMEndpoint)
+#define soundio_IID_IAUDIOCLIENT (&soundio_IID_IAudioClient)
+#define soundio_IID_IMMENDPOINT (&soundio_IID_IMMEndpoint)
 #define PKEY_DEVICE_FRIENDLYNAME (&PKEY_Device_FriendlyName)
 #define PKEY_AUDIOENGINE_DEVICEFORMAT (&PKEY_AudioEngine_DeviceFormat)
-#define CLSID_MMDEVICEENUMERATOR (&CLSID_MMDeviceEnumerator)
-#define IID_IAUDIOCLOCKADJUSTMENT (&IID_IAudioClockAdjustment)
-#define IID_IAUDIOSESSIONCONTROL (&IID_IAudioSessionControl)
-#define IID_IAUDIORENDERCLIENT (&IID_IAudioRenderClient)
-#define IID_IMMDEVICEENUMERATOR (&IID_IMMDeviceEnumerator)
-#define IID_IAUDIOCAPTURECLIENT (&IID_IAudioCaptureClient)
-#define IID_ISIMPLEAUDIOVOLUME (&IID_ISimpleAudioVolume)
+#define soundio_CLSID_MMDEVICEENUMERATOR (&soundio_CLSID_MMDeviceEnumerator)
+#define soundio_IID_IAUDIOCLOCKADJUSTMENT (&soundio_IID_IAudioClockAdjustment)
+#define soundio_IID_IAUDIOSESSIONCONTROL (&soundio_IID_IAudioSessionControl)
+#define soundio_IID_IAUDIORENDERCLIENT (&soundio_IID_IAudioRenderClient)
+#define soundio_IID_IMMDEVICEENUMERATOR (&soundio_IID_IMMDeviceEnumerator)
+#define soundio_IID_IAUDIOCAPTURECLIENT (&soundio_IID_IAudioCaptureClient)
+#define soundio_IID_ISIMPLEAUDIOVOLUME (&soundio_IID_ISimpleAudioVolume)
 #endif
 
 // Attempting to use the Windows-supplied versions of these constants resulted
@@ -835,7 +835,7 @@ static int refresh_devices(struct SoundIoPrivate *si) {
             IMMEndpoint_Release(rd.endpoint);
             rd.endpoint = NULL;
         }
-        if (FAILED(hr = IMMDevice_QueryInterface(rd.mm_device, IID_IMMENDPOINT, (void**)&rd.endpoint))) {
+        if (FAILED(hr = IMMDevice_QueryInterface(rd.mm_device, soundio_IID_IMMENDPOINT, (void**)&rd.endpoint))) {
             rd.device_shared->probe_error = SoundIoErrorOpeningDevice;
             rd.device_raw->probe_error = SoundIoErrorOpeningDevice;
             rd.device_shared = NULL;
@@ -886,7 +886,7 @@ static int refresh_devices(struct SoundIoPrivate *si) {
             IUnknown_Release(rd.audio_client);
             rd.audio_client = NULL;
         }
-        if (FAILED(hr = IMMDevice_Activate(rd.mm_device, IID_IAUDIOCLIENT,
+        if (FAILED(hr = IMMDevice_Activate(rd.mm_device, soundio_IID_IAUDIOCLIENT,
                         CLSCTX_ALL, NULL, (void**)&rd.audio_client)))
         {
             rd.device_shared->probe_error = SoundIoErrorOpeningDevice;
@@ -1096,8 +1096,8 @@ static void device_thread_run(void *arg) {
     struct SoundIoWasapi *siw = &si->backend_data.wasapi;
     int err;
 
-    HRESULT hr = CoCreateInstance(CLSID_MMDEVICEENUMERATOR, NULL,
-            CLSCTX_ALL, IID_IMMDEVICEENUMERATOR, (void**)&siw->device_enumerator);
+    HRESULT hr = CoCreateInstance(soundio_CLSID_MMDEVICEENUMERATOR, NULL,
+            CLSCTX_ALL, soundio_IID_IMMDEVICEENUMERATOR, (void**)&siw->device_enumerator);
     if (FAILED(hr)) {
         shutdown_backend(si, SoundIoErrorSystemResources);
         return;
@@ -1251,7 +1251,7 @@ static int outstream_do_open(struct SoundIoPrivate *si, struct SoundIoOutStreamP
     struct SoundIoDeviceWasapi *dw = &dev->backend_data.wasapi;
     HRESULT hr;
 
-    if (FAILED(hr = IMMDevice_Activate(dw->mm_device, IID_IAUDIOCLIENT,
+    if (FAILED(hr = IMMDevice_Activate(dw->mm_device, soundio_IID_IAUDIOCLIENT,
                     CLSCTX_ALL, NULL, (void**)&osw->audio_client)))
     {
         return SoundIoErrorOpeningDevice;
@@ -1299,7 +1299,7 @@ static int outstream_do_open(struct SoundIoPrivate *si, struct SoundIoOutStreamP
             }
             IUnknown_Release(osw->audio_client);
             osw->audio_client = NULL;
-            if (FAILED(hr = IMMDevice_Activate(dw->mm_device, IID_IAUDIOCLIENT,
+            if (FAILED(hr = IMMDevice_Activate(dw->mm_device, soundio_IID_IAUDIOCLIENT,
                             CLSCTX_ALL, NULL, (void**)&osw->audio_client)))
             {
                 return SoundIoErrorOpeningDevice;
@@ -1360,7 +1360,7 @@ static int outstream_do_open(struct SoundIoPrivate *si, struct SoundIoOutStreamP
     }
 
     if (outstream->name) {
-        if (FAILED(hr = IAudioClient_GetService(osw->audio_client, IID_IAUDIOSESSIONCONTROL,
+        if (FAILED(hr = IAudioClient_GetService(osw->audio_client, soundio_IID_IAUDIOSESSIONCONTROL,
                         (void **)&osw->audio_session_control)))
         {
             return SoundIoErrorOpeningDevice;
@@ -1377,13 +1377,13 @@ static int outstream_do_open(struct SoundIoPrivate *si, struct SoundIoOutStreamP
         }
     }
 
-    if (FAILED(hr = IAudioClient_GetService(osw->audio_client, IID_IAUDIORENDERCLIENT,
+    if (FAILED(hr = IAudioClient_GetService(osw->audio_client, soundio_IID_IAUDIORENDERCLIENT,
                     (void **)&osw->audio_render_client)))
     {
         return SoundIoErrorOpeningDevice;
     }
 
-    if (FAILED(hr = IAudioClient_GetService(osw->audio_client, IID_ISIMPLEAUDIOVOLUME,
+    if (FAILED(hr = IAudioClient_GetService(osw->audio_client, soundio_IID_ISIMPLEAUDIOVOLUME,
                     (void **)&osw->audio_volume_control)))
     {
         return SoundIoErrorOpeningDevice;
@@ -1765,7 +1765,7 @@ static int instream_do_open(struct SoundIoPrivate *si, struct SoundIoInStreamPri
     struct SoundIoDeviceWasapi *dw = &dev->backend_data.wasapi;
     HRESULT hr;
 
-    if (FAILED(hr = IMMDevice_Activate(dw->mm_device, IID_IAUDIOCLIENT,
+    if (FAILED(hr = IMMDevice_Activate(dw->mm_device, soundio_IID_IAUDIOCLIENT,
                     CLSCTX_ALL, NULL, (void**)&isw->audio_client)))
     {
         return SoundIoErrorOpeningDevice;
@@ -1813,7 +1813,7 @@ static int instream_do_open(struct SoundIoPrivate *si, struct SoundIoInStreamPri
             }
             IUnknown_Release(isw->audio_client);
             isw->audio_client = NULL;
-            if (FAILED(hr = IMMDevice_Activate(dw->mm_device, IID_IAUDIOCLIENT,
+            if (FAILED(hr = IMMDevice_Activate(dw->mm_device, soundio_IID_IAUDIOCLIENT,
                             CLSCTX_ALL, NULL, (void**)&isw->audio_client)))
             {
                 return SoundIoErrorOpeningDevice;
@@ -1871,7 +1871,7 @@ static int instream_do_open(struct SoundIoPrivate *si, struct SoundIoInStreamPri
     }
 
     if (instream->name) {
-        if (FAILED(hr = IAudioClient_GetService(isw->audio_client, IID_IAUDIOSESSIONCONTROL,
+        if (FAILED(hr = IAudioClient_GetService(isw->audio_client, soundio_IID_IAUDIOSESSIONCONTROL,
                         (void **)&isw->audio_session_control)))
         {
             return SoundIoErrorOpeningDevice;
@@ -1888,7 +1888,7 @@ static int instream_do_open(struct SoundIoPrivate *si, struct SoundIoInStreamPri
         }
     }
 
-    if (FAILED(hr = IAudioClient_GetService(isw->audio_client, IID_IAUDIOCAPTURECLIENT,
+    if (FAILED(hr = IAudioClient_GetService(isw->audio_client, soundio_IID_IAUDIOCAPTURECLIENT,
                     (void **)&isw->audio_capture_client)))
     {
         return SoundIoErrorOpeningDevice;
@@ -2186,7 +2186,7 @@ static inline struct SoundIoPrivate *soundio_MMNotificationClient_si(IMMNotifica
 static STDMETHODIMP soundio_MMNotificationClient_QueryInterface(IMMNotificationClient *client,
         REFIID riid, void **ppv)
 {
-    if (IS_EQUAL_IID(riid, &IID_IUnknown) || IS_EQUAL_IID(riid, &IID_IMMNotificationClient)) {
+    if (IS_EQUAL_IID(riid, &IID_IUnknown) || IS_EQUAL_IID(riid, &soundio_IID_IMMNotificationClient)) {
         *ppv = client;
         IUnknown_AddRef(client);
         return S_OK;
